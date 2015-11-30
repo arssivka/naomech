923043cb6acf50e077dcec5f14cbf8a0b00bf175
Arseniy Ivin <arssivka@yandex.ru>
2015-11-30 18:50:03 +0300

    Remote joints control was added to hardware

--- hardware/include/rd/hardware/RemoteJoints.h
+++ hardware/include/rd/hardware/RemoteJoints.h
@@ -11,10 +11,41 @@
 namespace rd {
     class RemoteJoints : public RPCModule {
     public:
-        RemoteJoints(boost::shared_ptr<Joints> joints);
+        RemoteJoints(boost::shared_ptr<Joints> &joints);
 
     private:
-        boost::shared_ptr<Joints> joints;
+        class KeysMethod : public RPCMethod {
+        public:
+            KeysMethod(boost::shared_ptr<Joints> &joints);
+
+            virtual void execute(xmlrpc_c::paramList const &paramList,
+                                 xmlrpc_c::value *const resultP);
+
+        private:
+            xmlrpc_c::value keys;
+        };
+
+        class PositionMethod : public RPCMethod {
+        public:
+            PositionMethod(boost::shared_ptr<Joints> &joints);
+
+            virtual void execute(xmlrpc_c::paramList const &paramList,
+                                 xmlrpc_c::value *const resultP);
+
+        private:
+            boost::shared_ptr<Joints> joints;
+        };
+
+        class HardnessMethod : public RPCMethod {
+        public:
+            HardnessMethod(boost::shared_ptr<Joints> &joints);
+
+            virtual void execute(xmlrpc_c::paramList const &paramList,
+                                 xmlrpc_c::value *const resultP);
+
+        private:
+            boost::shared_ptr<Joints> joints;
+        };
     };
 }
 
