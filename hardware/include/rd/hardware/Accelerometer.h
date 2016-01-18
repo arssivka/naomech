596a09c189dbc747c389ac3dc24a5531d1156dbb
Arseniy Ivin <arssivka@yandex.ru>
2016-01-18 14:22:10 +0300

    Accelerometer was added

--- hardware/include/rd/hardware/Accelerometer.h
+++ hardware/include/rd/hardware/Accelerometer.h
@@ -5,8 +5,34 @@
 #ifndef NAOMECH_ACCELEROMETER_H
 #define NAOMECH_ACCELEROMETER_H
 
-namespace rd {
+#include <alcommon/albroker.h>
+#include <rd/representation/SensorData.h>
+#include <alproxies/almemoryproxy.h>
+#include <alproxies/dcmproxy.h>
 
+namespace rd {
+    class Accelerometer {
+    public:
+        enum Key {
+            X, // What this is kind of pokemon?!
+            Y,
+            Z,
+            SENSOR_COUNT
+        };
+
+        Accelerometer(boost::shared_ptr<AL::ALBroker> broker);
+
+        const std::vector<std::string> &getKeys();
+
+        boost::shared_ptr<SensorData<double> > getAcceleration();
+
+    private:
+        std::vector<std::string> keys;
+        boost::shared_ptr<AL::ALMemoryProxy> mem;
+        boost::shared_ptr<AL::DCMProxy> dcm;
+
+        AL::ALValue sensors;
+    };
 }
 
 #endif //NAOMECH_ACCELEROMETER_H
