dbbdf94cf78e9895b46e446381b438c2a7cd1e7d
tekatod <ocnechlahim@mail.ru>
2016-05-05 17:04:25 +0300

    Kick ball demo

--- behavior/demos/kick_ball_demo.py
+++ behavior/demos/kick_ball_demo.py
@@ -17,6 +17,23 @@ robot.joints.hardness(0.8)
 pose_handler.set_pose("walking_pose", 1.0)
 walk = Walker(robot)
 
+stance = [310.0, 14.5, 100.0, 0.0, 0.0, 0.1]
+step = [0.75, 0.50, 15.0, 0.0, 70.0, -50.0, 70.0, 0.35, 70.0, 70.0, 0.35, 1.0]
+zmp = [0.0, 0.0, 20.0, 20.0, 0.01, 1.6]
+hack = [0.0, 0.0]
+sensor = [0.0,
+          0.0,
+          0.0,
+          0.0,
+          0.0,
+          0.0,
+          0.0,
+          0.0]
+stiff = [0.85, 0.7, 0.4, 0.3, 0.2, 0.2]
+odo = [1.0, 1.0, 1.5]
+arm = [0.3]
+robot.locomotion.gait(stance, step, zmp, hack, sensor, stiff, odo, arm)
+
 def ball_recognitor():
     robot.vision.updateFrame()
     ball = robot.vision.ballDetect()
@@ -53,3 +70,6 @@ def ball_kicker():
         pose_switcher.switch_to("prepare_left_kick", "walking_pose")
     else:
         pose_switcher.switch_to("prepare_right_kick", "walking_pose")
+
+ball_finder()
+ball_kicker()
