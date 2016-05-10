147e61dbe378f76d782c05b647d8a400d864328c
TekatoD <ocnechlahim@mail.ru>
2016-05-10 12:02:27 +0300

    Config

--- behavior/vision_test.py
+++ behavior/vision_test.py
@@ -19,7 +19,7 @@ import camera_calibator
 import time
 import numpy as np
 
-robot = Robot("192.168.1.64", "5469")
+robot = Robot("192.168.1.2", "5469")
 
 pose_handler = PoseHandler(robot, 30)
 pose.load_poses(pose_handler, "config/poses.json")
@@ -42,14 +42,14 @@ try:
         robot.vision.updateFrame()
         ball = robot.vision.ballDetect()
         lines = robot.vision.lineDetect()
-        cv2.imwrite('/home/nikitas/bases/real_image/' + str(i) + '.png', picturerbg)
+        cv2.imwrite('/home/tekatod/bases/real_image/' + str(i) + '.png', picturerbg)
         i += 1
 
         end = timeit.default_timer()
-        for line in lines:
-            cv2.line(picturerbg, (line['x1'], line['y1']), (line['x2'], line['y2']), (255, 0, 255))
-        cv2.rectangle(picturerbg, (ball['x'], ball['y']),
-                      (ball['x'] + ball['width'], ball['y'] + ball['height']), (0, 255, 255))
+        # for line in lines:
+        #     cv2.line(picturerbg, (line['x1'], line['y1']), (line['x2'], line['y2']), (255, 0, 255))
+        # cv2.rectangle(picturerbg, (ball['x'], ball['y']),
+        #               (ball['x'] + ball['width'], ball['y'] + ball['height']), (0, 255, 255))
         cv2.imshow('robotVis', picturerbg)
 
         cv2.waitKey(1000)
